d4bf64c24860247038b8b6f617f941c0cbc63000
HADDADA <zakaria.haddada@capgemini.com>
2019-10-11 16:15:56 +0100

    re adding SAOP Service Project

---
+++ SAOPService/target/generated-sources/jaxb/io/spring/guides/gs_producing_web_service/package-info.java
@@ -0,0 +1,9 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.2 
+// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2019.10.11 à 02:38:43 PM WAT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package io.spring.guides.gs_producing_web_service;
